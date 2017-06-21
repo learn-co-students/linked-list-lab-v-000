065530545cdacae930481b1f8fa9338df3bf56a2
Enoch2k2 <enoch2k2@hotmail.com>
2017-06-21 14:23:35 -0500

    Done.

--- index.js
+++ index.js
@@ -2,76 +2,56 @@ function getName(node){
   return node.name;
 }
 
-function headNode(linkedList, collection){
-  return collection[linkedList];
+function headNode(head, collection){
+  return collection[head];
 }
 
 function next(node, collection){
   return collection[node.next];
 }
 
-function nodeAt(index, linkedList, collection){
-  var node = collection[linkedList];
+function nodeAt(index, head, collection){
+  var node = collection[head];
   for (var i = 0; i < index; i++) {
-    node = next(node, collection)
+    node = next(node, collection);
   }
   return node;
 }
 
-function addressAt(index, linkedList, collection){
-  var node = collection[linkedList];
+function addressAt(index, head, collection){
   if(index == 0){
-    return linkedList;
+    return head;
   } else {
-    for (var i = 0; i < index-1; i++) {
-      node = next(node, collection);
-    }
-    return node.next;
+    var prevNode = nodeAt(index-1, head, collection);
+    return prevNode.next;
   }
 }
 
-function indexAt(n, collection, linkedList){
-  var index = 0;
-  var node = collection[linkedList];
-  while(index < Object.keys(collection).length){
-    if(node.name === n.name){
-      return index;
+function indexAt(node, collection, head){
+  var current = headNode(head, collection);
+  for (var i = 0; i < Object.keys(collection).length; i++) {
+    if(current === node){
+      return i;
     } else {
-      node = collection[node.next];
-      index++;
+      current = next(current, collection);
     }
   }
 }
 
 function insertNodeAt(index, address, head, collection){
-  // we need to get the previous node
-  // we will need to get the next node after the change
   var prevNode = nodeAt(index-1, head, collection);
   var nextNode = nodeAt(index, head, collection);
-  // we get the next address so that we can point the node we are inserting to that address
   var nextAddress = addressAt(index, head, collection);
-  // we set the previous node's address to point to the node we are inserting
+
+  var node = collection[address];
   prevNode.next = address;
-  // we create the new node with the address set to it.
-  var newNode = collection[address];
-  // we set the next value to the next address to point to the next node in line
-  newNode.next = nextAddress;
+  node.next = nextAddress;
 }
 
 function deleteNodeAt(index, head, collection){
-  // start with the head node
-  var node = headNode(head, collection);
-  // keep track of the previous node
-  var previous;
-
-  for (var i = 0; i < index; i++) {
-    // stair case it down keeping track of the previous nodes until we reach the index
-    previous = node;
-    // reset the node to the next node in the collection
-    node = next(node, collection);
-  }
-  // change the previous node's next address to the node's address after the node we are deleting so it's connected
-  previous.next = node.next;
-  // delete the node;
-  delete collection[node];
+  var prevNode = nodeAt(index-1, head, collection);
+  var current = nodeAt(index, head, collection);
+  var nextAddress = addressAt(index+1, head, collection);
+  prevNode.next = nextAddress;
+  delete collection[current]
 }
