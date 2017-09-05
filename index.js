0c96f1c5fac5fed13bb21fc933ea4f05d774256b
Andrew Bonner <andrewbonner92@gmail.com>
2017-09-05 19:58:41 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,55 @@
+function getName(node) {
+  return node.name
+}
+
+function headNode(head, collection) {
+  return collection[head]
+}
+
+function next(node, collection) {
+  return collection[node.next]
+}
+
+function nodeAt(index, head, collection) {
+  let node = collection[head]
+  for (let i = 0; i < index; i++) {
+    node = next(node, collection)
+  }
+  return node
+}
+
+function addressAt(index, head, collection) {
+  if (index > 0) {
+    return nodeAt(index - 1, head, collection).next
+  }
+  return head
+}
+
+function indexAt(node, collection, head) {
+  let counter = 0
+  let currentNode = collection[head]
+  if (node !== currentNode) {
+    currentNode = collection[currentNode.next]
+    counter++
+  }
+  return counter
+}
+
+function insertNodeAt(index, newNode, head, collection) {
+  if (index > 0) {
+    let previousNode = nodeAt(index - 1, head, collection)
+    collection[newNode].next = previousNode.next
+    previousNode.next = newNode
+  } else {
+    collection[newNode].next = addressAt(index, head, collection)
+  }
+}
+
+function deleteNodeAt(index, head, collection) {
+  if (index > 0) {
+    let previousNode = nodeAt(index - 1, head, collection)
+    previousNode.next = next(previousNode, collection).next
+  } else {
+    nodeAt(index, head, collection).next = null
+  }
+}
