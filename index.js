9fc7ccd5f340aa5f5e8390408531026207138089
tpstar <hanlee12@gmail.com>
2017-10-17 21:30:13 -0400

    done

--- index.js
+++ index.js
@@ -17,3 +17,43 @@ function nodeAt(n, linkedList, collection) {
   }
   return node;
 }
+
+function addressAt(n, linkedList, collection) {
+  var address = linkedList;
+  if (n > 0) {
+     address = nodeAt(n-1, linkedList, collection).next
+  }
+  return address;
+}
+
+function indexAt(targetnode, collection, linkedList) {
+  var index = 0;
+  var node = collection[linkedList];
+    while (node.next) {
+      if (node === targetnode) {
+        return index;
+      }
+      node = next(node, collection);
+      index++;
+    }
+}
+
+function insertNodeAt(n, address, linkedList, collection) {
+  let newNode = collection[address];
+  if (n === 0) {
+    newNode.next = linkedList;
+    linkedList = address;
+  } else {
+    newNode.next = nodeAt(n-1, linkedList, collection).next;
+    nodeAt(n-1, linkedList, collection).next = address;
+  }
+}
+
+function deleteNodeAt(n, linkedList, collection) {
+  if (n === 0) {
+    linkedList = addressAt(1, linkedList, collection);
+  } else {
+    nodeAt(n-1, linkedList, collection).next = addressAt(n+1, linkedList, collection);
+  }
+  delete nodeAt(n, linkedList, collection)
+}
