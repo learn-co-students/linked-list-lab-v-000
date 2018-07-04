2e4ef69b605d1b882868d421a7438e9acb3518a8
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-07-04 06:48:32 +0000

    Done.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function headNode(linkedList, collection){
 }
 
 function next(node, collection){
-    return collection[node[1]]
+    return collection[node['next']]
 }
 
 function nodeAt(index, linkedList, collection){
@@ -16,6 +16,7 @@ function nodeAt(index, linkedList, collection){
 
     while (counter != index){
         node = next(node, collection)
+        counter += 1
     }
 
     return node
@@ -39,22 +40,32 @@ function indexAt(node, collection, linkedList){
         let currentNode = collection[linkedList]
 
         while (currentNode !== node){
-            currentNode = next(currentNode)
+            currentNode = next(currentNode, collection)
             index += 1
         }
 
         return index
     }
+
 }
 
-function insertNodeAt(index, address, linkedList, collection){
-    if (0 === index){
+function insertNodeAt(spot, address, linkedList, collection){
+    if (0 === spot){
         collection[address]['next'] = linkedList
     }
 
     else {
-        let previousNode = nodeAt(index - 1, linkedList, collection)
-        collection[address]['next'] = previousNode['address']
-        collection[getName(previousNode)]['address'] = address
+        let previousNode = nodeAt(spot - 1, linkedList, collection)
+
+        collection[address]['next'] = previousNode['next']
+        collection[addressAt(spot - 1, linkedList, collection)]['next'] = address
     }
+
+}
+
+function deleteNodeAt(index, linkedList, collection){
+  let doomedNode = nodeAt(index, linkedList, collection)
+  if (index > 0) {
+    collection[addressAt(index - 1, linkedList, collection)]['next'] = doomedNode['next']
+  }
 }
