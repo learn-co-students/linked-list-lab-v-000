5dc258949eec360b3fa56cc941be8fbd669a477d
Dillorom <dabdulloeva@gmail.com>
2018-11-07 11:13:40 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,51 @@
+
+const collection = {firstNode: {name: 'Alexandra', next: 'secondNode'},
+ secondNode: {name: 'Kirstin', next: 'thirdNode'}, 
+ thirdNode: {name: 'Juliet', next: 'fourthNode'},
+ fourthNode: {name: 'Timmy', next: 'fifthNode'},
+ fifthNode: {name: 'Jacob'}
+}
+
+
+function getName(node){
+     return node.name
+
+}
+
+function headNode(linkedList, collection){
+    return collection[linkedList]
+    
+}
+
+function next(node, collection){
+    let nextAddress = node.next
+    return collection[`${nextAddress}`]
+}
+
+function nodeAt(index, linkedList, collection){
+    let currentNode = headNode(linkedList, collection)
+    for (let i = 0; i < index; i++){
+        currentNode = next(currentNode, collection)
+    }
+    return currentNode
+
+}
+
+function addressAt(index, linkedList, collection){
+    if(index == 0){
+      return linkedList
+    } else {
+      let node = nodeAt(index-1, linkedList, collection)
+      return node.next
+    }
+  }
+
+  function indexAt(node, collection, linkedList){
+    let currentNode = headNode(linkedList, collection);
+    let currentIdx = 0
+    while(currentNode != node){
+      currentIdx++
+      currentNode = next(currentNode, collection)
+    }
+    return currentIdx
+  }
