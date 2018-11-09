701fe0a7d91b65f946e0058947929dca1573dd41
Dillorom <dabdulloeva@gmail.com>
2018-11-09 10:38:15 -0800

    Done.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function getName(node){
 }
 
 function headNode(linkedList, collection){
+  let head = linkedList;
     return collection[linkedList]
     
 }
@@ -48,4 +49,28 @@ function addressAt(index, linkedList, collection){
       currentNode = next(currentNode, collection)
     }
     return currentIdx
+  }
+
+  function insertNodeAt(index, newNodeAddress, linkedList, collection){
+    let previousNode = nodeAt(index - 1, linkedList, collection)
+    let subsequentNode = nodeAt(index, linkedList, collection)
+  
+    let previousNodeIdx = indexAt(previousNode, collection, linkedList)
+    let subsequentNodeIdx = indexAt(subsequentNode, collection, linkedList)
+    let previousNodeAddress = addressAt(previousNode, linkedList, collection)
+    let subsequentNodeAddress = addressAt(subsequentNode, linkedList, collection)
+    previousNode.next = newNodeAddress
+    let newNode = collection[newNodeAddress]
+    newNode.next = subsequentNodeAddress
+  }
+  
+  
+  function deleteNodeAt(index, linkedList, collection){
+    let previousNode;
+    let currentNode = headNode(linkedList, collection);
+    for(let i = 0; i < index; i++){
+       previousNode = currentNode
+       currentNode = next(currentNode, collection);
+    }
+    previousNode.next = currentNode.next
   }
