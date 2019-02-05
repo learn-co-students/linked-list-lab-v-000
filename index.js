ebddae0d93b377cff0a2b0536ba967a0eec97999
anikonchuk <anikonch@gmail.com>
2019-02-05 14:14:21 -0500

    Done.

--- index.js
+++ index.js
@@ -18,3 +18,37 @@ function nodeAt(index, startAddress, collection) {
   }
   return node
 }
+
+function addressAt(index, linkedList, collection){
+  if(index == 0){
+    return linkedList
+  } else {
+    let node = nodeAt(index-1, linkedList, collection)
+    return node.next
+  }
+}
+
+function indexAt(node, collection, startAddress) {
+  let currentNode = headNode(startAddress, collection);
+  let currentIndex = 0
+  while (currentNode != node) {
+    currentIndex += 1
+    currentNode = next(currentNode, collection)
+  }
+  return currentIndex
+}
+
+function insertNodeAt(index, newNodeAddress, startAddress, collection) {
+  let previousNode = nodeAt((index-1), startAddress, collection)
+  let subsequentNode = nodeAt(index, startAddress, collection)
+  let subsequentNodeAddress = addressAt(subsequentNode, startAddress, collection)
+  previousNode.next = newNodeAddress
+  let newNode = collection[newNodeAddress]
+  newNode.next = subsequentNodeAddress
+}
+
+function deleteNodeAt(index, startAddress, collection) {
+  let previousNode = nodeAt((index-1), startAddress, collection)
+  let deletedNode = nodeAt(index, startAddress, collection)
+  previousNode.next = deletedNode.next
+}
