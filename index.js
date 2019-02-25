126ff70c209458970e68a121fda6dbc32e92aea3
VictoriaMeng <1ststarofthenight@gmail.com>
2019-02-25 11:19:32 -0800

    Shorten next() by 10 lines All nodes have 'next' key No need for control flow/loops to parse for 'next'

--- index.js
+++ index.js
@@ -7,19 +7,20 @@ function headNode(linkedList, collection) {
 }
 
 function next(head, collection) {
-  var addresses = Object.keys(collection)
-  var lastIndex = addresses.length - 1
-  if (head == collection[addresses[lastIndex]]) {
-    return collection[addresses[0]]
-  } else {
-    for (var i = 0; i <= lastIndex; i++) {
-      if (collection[addresses[i]] == head) {
-        return collection[addresses[i + 1]]
-      }
-    }
-  }
+  return collection[head.next]
+  // var addresses = Object.keys(collection)
+  // var lastIndex = addresses.length - 1
+  // if (head == collection[addresses[lastIndex]]) {
+  //   return collection[addresses[0]]
+  // } else {
+  //   for (var i = 0; i <= lastIndex; i++) {
+  //     if (collection[addresses[i]] == head) {
+  //       return collection[addresses[i + 1]]
+  //     }
+  //   }
+  // }
 }
 
-function nodeAt() {
-  
+function nodeAt(index, linkedList, collection) {
+
 }
