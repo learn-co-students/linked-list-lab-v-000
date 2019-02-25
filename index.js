94608c451f7bae4c402877c914092aa1ff1c9b83
VictoriaMeng <1ststarofthenight@gmail.com>
2019-02-24 20:14:38 -0800

    Define empty function getName()

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ let collection = {rnadnm: {name: 'Alexandra', next: 'masjdrandm'},
 masjdrandm: {name: 'Kirstin', next: 'ntrandm'}, 
 ntrandm: {name: 'Juliet', next: null}
 }
+
+function getName() {
+  
+}
