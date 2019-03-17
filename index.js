7e0f1e36fef316976297baebf3eb3e6ea89e5a1f
Jack Yeung <jyeung.se@gmail.com>
2019-03-16 23:02:34 -0400

    Done.

--- index.js
+++ index.js
@@ -19,3 +19,48 @@ const nodeAt = (index, linkedList, collection) => {
   }
   return targetNode
 }
+
+const addressAt = (index, linkedList, collection) => {
+  if (index == 0) {
+    return linkedList
+  } else {
+    let node = nodeAt(index-1, linkedList, collection)
+    return node.next
+  }
+}
+
+const indexAt = (node, collection, linkedList) => {
+  let currentNode = headNode(linkedList, collection)
+  let currentIndex = 0
+
+  while (currentNode != node) {
+    currentIndex++
+    currentNode = next(currentNode, collection)
+  }
+  return currentIndex
+}
+
+const insertNodeAt = (index, newNodeAddress, linkedList, collection) => {
+  let previousNode = nodeAt(index-1, linkedList, collection)
+  let followingNode = nodeAt(index, linkedList, collection)
+
+  let previousNodeIndex = indexAt(previousNode, collection, linkedList)
+  let followingNodeIndex = indexAt(followingNode, collection, linkedList)
+  let previousNodeAddress = addressAt(previousNode, linkedList, collection)
+  let followingNodeAddress = addressAt(followingNode, linkedList, collection)
+
+  previousNode.next = newNodeAddress
+  let newNode = collection[newNodeAddress]
+  newNode.next = followingNodeAddress
+}
+
+const deleteNodeAt = (index, linkedList, collection) => {
+  let previousNode
+  let currentNode = headNode(linkedList, collection)
+
+  for (let i = 0; i < index; i++) {
+    previousNode = currentNode
+    currentNode = next(currentNode, collection)
+  }
+  previousNode.next = currentNode.next
+}
