09107fac317ae5935fe001b511709c8bb067cda2
Paris Dolfman <parisdolfman@gmail.com>
2019-04-10 19:38:36 +0000

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ function next(node, collection) {
   let nextAddress = node.next
   return collection[nextAddress]
 }
+
+function nodeAt(index, linkedList, collection) {
+  let currentNode = headNode(linkedList, collection)
+  for (let counter = 0; counter < index; counter++) {
+    currentNode = next(currentNode, collection)
+  }
+  return currentNode
+}
