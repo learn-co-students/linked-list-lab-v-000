6711149eaa4655ea67e3371fe5bcc587f1a8e6c4
rcrdlbl <bluh@outlook.com>
2019-04-16 20:25:14 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function getName(node) {
 function headNode(linkedList, collection) {
   return collection[linkedList]
 }
+
+function next(node, collection) {
+  return collection[node.next]
+}
+
+function nodeAt(num, linkedList, collection) {
+  let node = collection[linkedList]
+  for (let i = 0; i < num; i++) {
+    node = next(node, collection)
+  }
+
+  return node
+}
