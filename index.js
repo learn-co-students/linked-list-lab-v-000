26a2245f03cd6111d5911fa11d61957382eace44
wali89 <wali.rahman89@yahoo.com>
2019-05-21 20:29:54 -0400

    complete

--- index.js
+++ index.js
@@ -38,3 +38,26 @@ function indexAt(node, collection, linkedList){
   }
   return currentIdx
 }
+function insertNodeAt(index, newNodeAddress, linkedList, collection){
+  let previousNode = nodeAt(index - 1, linkedList, collection)
+  let subsequentNode = nodeAt(index, linkedList, collection)
+
+  let previousNodeIdx = indexAt(previousNode, collection, linkedList)
+  let subsequentNodeIdx = indexAt(subsequentNode, collection, linkedList)
+  let previousNodeAddress = addressAt(previousNode, linkedList, collection)
+  let subsequentNodeAddress = addressAt(subsequentNode, linkedList, collection)
+  previousNode.next = newNodeAddress
+  let newNode = collection[newNodeAddress]
+  newNode.next = subsequentNodeAddress
+}
+
+
+function deleteNodeAt(index, linkedList, collection){
+  let previousNode;
+  let currentNode = headNode(linkedList, collection);
+  for(let i = 0; i < index; i++){
+     previousNode = currentNode
+     currentNode = next(currentNode, collection);
+  }
+  previousNode.next = currentNode.next
+}
