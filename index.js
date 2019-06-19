d5fac893f0cf03c2291f95c16b136555e0a43751
Wali Rahman <wali.rahman89@yahoo.com>
2019-06-19 01:22:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-function getName(node) {
-  return node.name
-}
-
-function headNode(linkedList, collection){
-  return collection[linkedList]
+function getName(node){
+  return node.name;
 }
 
 function next(node, collection){
   let nextAddress = node.next
   return collection[`${nextAddress}`]
-}
-
-function nodeAt(index, linkedList, collection){
-  let currentNode = headNode(linkedList, collection);
-  for(let i = 0; i < index; i++){
-     currentNode = next(currentNode, collection);
-  }
-
-  return currentNode;
 }
 
 function addressAt(index, linkedList, collection){
@@ -29,6 +16,19 @@ function addressAt(index, linkedList, collection){
   }
 }
 
+function headNode(linkedList, collection){
+  return collection[linkedList]
+}
+
+function nodeAt(index, linkedList, collection){
+  let currentNode = headNode(linkedList, collection);
+  for(let i = 0; i < index; i++){
+     currentNode = next(currentNode, collection);
+  }
+
+  return currentNode;
+}
+
 function indexAt(node, collection, linkedList){
   let currentNode = headNode(linkedList, collection);
   let currentIdx = 0
@@ -38,6 +38,8 @@ function indexAt(node, collection, linkedList){
   }
   return currentIdx
 }
+
+
 function insertNodeAt(index, newNodeAddress, linkedList, collection){
   let previousNode = nodeAt(index - 1, linkedList, collection)
   let subsequentNode = nodeAt(index, linkedList, collection)
