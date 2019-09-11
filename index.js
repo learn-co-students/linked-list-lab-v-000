aad98e2279bfc3a23877dbe2966f7f3b19501adc
christianavdm <christianavdm@gmail.com>
2019-09-11 13:28:15 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function getName(node) {
+    return node["name"]
+}
+
+function headNode(linkedList, collection) {
+    return collection[linkedList]
+}
+
+function next(node, collection){
+    let nextAddress = node["next"] 
+    return collection[nextAddress]
+  }
+
+function nodeAt(index, head, collection){
+    let node = collection[head];
+    let i; 
+    for(i = 0; i < index; i++){
+        node = next(node, collection);
+    }
+
+    return node;
+}
+
+function addressAt(index, head, collection){
+    let nodeAddress = head; 
+    let node = collection[head];
+    let i; 
+    for(i = 0; i < index; i++){
+        nodeAddress = node["next"];
+        node = next(node, collection);
+    }
+
+    return nodeAddress;
+}
